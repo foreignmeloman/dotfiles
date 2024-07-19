0410ea08509c38ca67653aadc538030f12e578e0
foreignmeloman <foreignmeloman@gmail.com>
2024-07-19 13:52:48 +0400

    Integrate pyinfra

---
+++ pyinfra/deploy.py
@@ -0,0 +1,54 @@
+from pyinfra import host
+from pyinfra.facts.server import LinuxName, Home, KernelVersion
+from pyinfra.operations import zypper, files, python, systemd
+
+import utils
+
+if host.get_fact(KernelVersion).count('microsoft') == 0:
+    systemd.service(
+        name='Enable ssh-agent user service',
+        service='ssh-agent.service',
+        user_mode=True,
+        enabled=True,
+    )
+
+FONTS_DIR = f'{host.get_fact(Home)}/.local/share/fonts'
+FONTS_ARCHIVES_DIR = f'{FONTS_DIR}/archives'
+
+files.directory(
+    name='Create font archives directory',
+    path=FONTS_ARCHIVES_DIR,
+)
+
+for font_name in ['Hack.tar.xz', 'RobotoMono.tar.xz']:
+    font = utils.NerdFont(font_name, 'v3.2.1')
+    font_file = f'{FONTS_ARCHIVES_DIR}/{font.basename}'
+    fonts_downloaded = files.download(
+        name=f'Download font {font_name}',
+        src=font.url,
+        dest=font_file,
+        sha256sum=font.sha256,
+    )
+
+    python.call(
+        name=f'Extract font {font_name}',
+        function=utils.extract_tarfile,
+        file=font_file,
+        dest=FONTS_DIR,
+        exclude=['LICENSE.*', 'README.md'],
+    )
+
+
+if host.get_fact(LinuxName) == 'openSUSE Tumbleweed':
+    zypper.packages(
+        name='Install required packages',
+        packages=[
+            'btop',
+            'gdu',
+            'k9s',
+            'lf',
+            'tmux',
+            'yq',
+        ],
+        _sudo=True,
+    )
---
+++ pyinfra/utils.py
@@ -0,0 +1,65 @@
+import fnmatch
+import os
+import tarfile
+import urllib.request
+
+from pyinfra import logger
+
+
+def _exclude_tarfile_members(
+    tar_archive: tarfile.TarFile = None,
+    exclude: list = None,
+):
+    excluded_members = [
+        f for e in exclude for f in fnmatch.filter(tar_archive.getnames(), e)
+    ]
+    for tarinfo in tar_archive:
+        if tarinfo.name not in excluded_members:
+            yield tarinfo
+
+
+def extract_tarfile(file: str, dest: str, exclude: list = None):
+    if tarfile.is_tarfile(file):
+        with tarfile.open(file) as fh:
+            fh.extractall(
+                path=dest,
+                members=_exclude_tarfile_members(fh, exclude),
+            )
+    else:
+        raise Exception(f'{file} is not a tar archive!')
+
+
+class NerdFont:
+    BASE_URL = 'https://github.com/ryanoasis/nerd-fonts/releases/download'
+
+    def __init__(self, name: str, version: str) -> None:
+        self.__name = name
+        self.__version = version
+
+    @property
+    def sha256(self) -> str:
+        sha256_url = f'{self.BASE_URL}/{self.__version}/SHA-256.txt'
+        sha256_file = f'/tmp/NF-SHA256-{self.__version}.txt'
+        if os.path.isfile(sha256_file):
+            with open(sha256_file, 'r') as fh:
+                data = fh.read()
+        else:
+            with urllib.request.urlopen(sha256_url) as response:
+                data = response.read().decode().strip()
+                with open(sha256_file, 'w') as fh:
+                    fh.write(data)
+        data_parsed = [line.split() for line in data.split('\n')]
+        return list(
+            filter(
+                lambda x: x[1] == self.__name,
+                data_parsed,
+            )
+        )[0][0]
+
+    @property
+    def url(self) -> str:
+        return f'{self.BASE_URL}/{self.__version}/{self.__name}'
+
+    @property
+    def basename(self) -> str:
+        return self.__name
